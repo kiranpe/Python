62378f86b0b62fe1b32bf8ebe6337ad0cb7698bd
kiranpe <kiranwls2011@gmail.com>
2021-05-11 17:29:06 +0530

    single file for ELB Tags

--- Create_TAG/ELB_TAG_Change.py
+++ Create_TAG/ELB_TAG_Change.py
@@ -9,29 +9,41 @@ os.environ['AWS_DEFAULT_REGION'] = "us-east-2"
 def lambda_handler():
       client = boto3.client('elbv2')
       
-      system = 'test'
-      CreatedBy = "Kiran P"
-      layer_TAG = 'web'
-      Type = 'alb'
-      Name_TAG = system + "-" + layer_TAG + "-" + Type
+      ARNs = 'arn:aws:elasticloadbalancing:us-east-2:901593578477:loadbalancer/app/test/b7a4102517408d86'
       
-      ARNs = ['arn:aws:elasticloadbalancing:us-east-2:901593578477:loadbalancer/app/testing/ab807acac88ae960', 'arn:aws:elasticloadbalancing:us-east-2:901593578477:loadbalancer/app/test/3c6b2b1b80eda0e6']
+      response = client.add_tags(ResourceArns=[ARNs],
+         Tags=[{'Key': 'System', 'Value': 'test',}, {'Key': 'CreatedBy', 'Value': 'Kiran',}, {'Key': 'Type', 'Value': 'alb',}, {'Key': 'Layer', 'Value': 'app'},
+               {'Key': 'Env', 'Value': 'testenv'},
+         ]
+      )
       
+      elb = client.describe_tags(ResourceArns=[ARNs],)
       
-      for arn in ARNs:
-        response = client.add_tags(ResourceArns=[arn],
-          Tags=[
-            {
-              'Key': 'Name',
-              'Value': Name_TAG,
-            },
-            {
-              'Key': 'Created By',
-              'Value': CreatedBy,
-            },
-          ],
-        )
-        
-        print(response)
+      for tagvalues in elb['TagDescriptions']:
+        for tag in tagvalues['Tags']:
+         if tag['Key'] == 'CreatedBy':
+            createdby = tag['Value']
+                
+         if tag['Key'] == 'Type':
+            type_tag = tag['Value']
+                
+         if tag['Key'] == 'System':
+            system = tag['Value']
+         
+         if tag['Key'] == 'Layer':
+            layer = tag['Value']
+         
+         if tag['Key'] == 'Env':
+            env = tag['Value']
+                
+      Name_Tag = env + '-' + system + '-' + layer + '-' + type_tag
+      
+      print()
+      print(Name_Tag)
+      print()
+      
+      response = client.add_tags(ResourceArns=[ARNs], Tags=[{'Key': 'Name', 'Value': Name_Tag,},])
+      
+      print(response)
 
 lambda_handler()
