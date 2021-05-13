3653350442094307c7b2716d9bc198e709fe4bd7
kiranpe <kiranwls2011@gmail.com>
2021-05-13 15:30:16 +0530

    tags creation for EBS volumes script && updated Readme file

---
+++ Create_TAG/EBS_Tag.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import boto3
+import os
+
+os.environ['AWS_PROFILE'] = "DevOps"
+os.environ['AWS_DEFAULT_REGION'] = "us-east-2"
+
+def lambda_handler():
+
+     ec2 = boto3.resource('ec2')
+
+     ids = ('vol-00568fda8f58be6c5', 'vol-0b1b887e556dc51c2')
+
+     for vol_id in ids:
+
+       volume = ec2.Volume(vol_id)
+       
+       tag = volume.create_tags(
+                Tags = [{"Key": "Env", "Value": "Test"},]
+       )
+ 
+       print(tag)
+
+lambda_handler()
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Python
 This repo is for Python
+
+Created some lambda functions using python to create tags and ssh to instance 
