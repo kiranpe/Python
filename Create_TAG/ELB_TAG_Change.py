2dc8fdb290b9eadc04dd43a1cf6cebd0516898f9
kiranpe <kiranwls2011@gmail.com>
2021-05-13 15:33:50 +0530

    updated for loop for multiple elbs

--- Create_TAG/ELB_TAG_Change.py
+++ Create_TAG/ELB_TAG_Change.py
@@ -7,17 +7,21 @@ os.environ['AWS_PROFILE'] = "DevOps"
 os.environ['AWS_DEFAULT_REGION'] = "us-east-2"
 
 def lambda_handler():
-      client = boto3.client('elbv2')
+    client = boto3.client('elbv2')
       
-      ARNs = 'arn:aws:elasticloadbalancing:us-east-2:901593xxxxxxx:loadbalancer/app/test/b7a4102517408d86'
+    ARNs = 'arn:aws:elasticloadbalancing:us-east-2:901593xxxxxxx:loadbalancer/app/test/b7a4102517408d86'
       
-      response = client.add_tags(ResourceArns=[ARNs],
-         Tags=[{'Key': 'System', 'Value': 'test',}, {'Key': 'CreatedBy', 'Value': 'Kiran',}, {'Key': 'Type', 'Value': 'alb',}, {'Key': 'Layer', 'Value': 'app'},
+    for arn in ARNs:  
+      response = client.add_tags(ResourceArns=[arn],
+         Tags=[{'Key': 'System', 'Value': 'test',}, 
+               {'Key': 'CreatedBy', 'Value': 'Kiran',}, 
+               {'Key': 'Type', 'Value': 'alb',}, 
+               {'Key': 'Layer', 'Value': 'app'},
                {'Key': 'Env', 'Value': 'testenv'},
          ]
       )
       
-      elb = client.describe_tags(ResourceArns=[ARNs],)
+      elb = client.describe_tags(ResourceArns=[arn],)
       
       for tagvalues in elb['TagDescriptions']:
         for tag in tagvalues['Tags']:
