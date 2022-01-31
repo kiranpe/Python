d8a469aaa6f18d53f40c309672b50d4a629a7bbb
kiranpe <kiranwls2011@gmail.com>
2022-01-31 22:32:08 +0530

    Read data from file and load into variables

--- string/test.py
+++ string/test.py
@@ -9,12 +9,12 @@ with open("data.txt",'r') as file:
   data.append(line.replace("\n", "").split(','))
 
  for idx in range(len(data)):
-    one,two,three,four = data[idx]
+    lst = data[idx]
   
-    name = str(one).strip()
-    email = str(two).strip() 
-    marketvalue = str(three).strip()
-    role = str(four).strip()
+    name = lst[0].strip()
+    email = lst[1].strip() 
+    marketvalue = lst[2].strip()
+    role = lst[3].strip()
    
     idx += 1 
     print(name)
