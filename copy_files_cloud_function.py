1670ec8a2b0a471643647b4850468a439dde6358
kiranpe <36789502+kiranpe@users.noreply.github.com>
2022-05-17 11:03:21 +0530

    Update copy_files_cloud_function.py

--- copy_files_cloud_function.py
+++ copy_files_cloud_function.py
@@ -8,12 +8,12 @@ from google.cloud import storage
 def copy_cloud_function():
     todayDate = os.popen("date +'%Y-%m-%d'").read().strip()
     print(todayDate)
-    bucket_name = "seg-cmp-ods-us"
+    bucket_name = "source-bucket"
     storage_client = storage.Client()
     source_bucket = storage_client.bucket(bucket_name)
-    destination_bucket_name = "seg-cmp-ods-us"
+    destination_bucket_name = "dest-bucket"
     destination_bucket = storage_client.bucket(destination_bucket_name)
-    list_of_blobs = source_bucket.list_blobs(prefix='ods/requests/')
+    list_of_blobs = source_bucket.list_blobs(prefix='requests/')
     list_of_files = []
     for x in list_of_blobs:
       if str(x.name).endswith('.json'):
@@ -24,9 +24,9 @@ def copy_cloud_function():
        latestblob = list_of_files[idx]['name']
 
        blob = os.popen("echo {} | cut -d '/' -f3".format(latestblob)).read().strip()
-       blob_name = f"ods/requests/{blob}"
+       blob_name = f"requests/{blob}"
        source_blob = source_bucket.blob(blob_name)
-       destination_blob_name = f"ods/requests/test/{blob}"
+       destination_blob_name = f"requests/test/{blob}"
        blob_copy = source_bucket.copy_blob(
          source_blob, destination_bucket, destination_blob_name
        )
